efb1521961f1e39b23e5e03c1b1c7dd99eb96132
Luca <eto.lucafrancesco@gmail.com>
2018-12-10 16:22:57 +0000

    stack the error

--- testFramework.js
+++ testFramework.js
@@ -1,15 +1,15 @@
 function Test () {
-
 }
 
 Test.prototype.test_equal = function (argument, expectedValue) {
   try {
-    if (isNaN(expectedValue)) throw "expected value is not a number"
-    if (argument !== expectedValue) throw "doesnt equal the expected value"
+    if (isNaN(expectedValue)) throw new Error ("expected value is not a number")
+    if (argument !== expectedValue) throw new Error ("doesnt equal the expected value")
     if (argument === expectedValue) console.log("%cPass", "color: green")
     }
   catch(err) {
-    console.log("%cError - " + err, "color: red")
+    console.log(err.stack)
+    console.log("%c" + err.stack, "color: red")
   }
 }
 
@@ -19,7 +19,6 @@ Test.prototype.test = function(func, message, argument, expectedvalue) {
     this.test_equal(argument, expectedvalue)
   }
 }
-
 
 var run = new Test();
 var square = new Square(2)
