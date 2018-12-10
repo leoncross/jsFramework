0be2ddcf206fcc6a692a8fca754e8da7473c6bb8
leoncross <leon@lscross.co.uk>
2018-12-10 14:16:43 +0000

    added useful error logging, created

--- testFramework.js
+++ testFramework.js
@@ -3,15 +3,33 @@ function Test () {
 }
 
 Test.prototype.comparison = function (argument1, expectedvalue) {
-  if (argument1 === expectedvalue){
-    console.log('Pass')
-  } else {
-    console.log('Fail')
+  try {
+    if (isNaN(expectedvalue)) throw "expected value is not a number"
+    if (argument1 === expectedvalue) console.log('Pass')
+    }
+  catch(err) {
+    console.log('Error - ' + err)
   }
 }
 
 
-var square = new Square(2)
-var test = new Test ()
-test.comparison(square.area(), 4)
-test.comparison(square.perimeter(), 8)
+Test.prototype.tests = function() {
+  var square = new Square(2)
+  this.comparison(square.area(), 4)
+  this.comparison(square.area(), "four")
+}
+
+
+var run = new Test();
+run.tests();
+//
+// var square = new Square(2)
+// test.comparison(square.area(), "four")
+
+// console.log(isNaN(4))
+
+// 1) Throw an error, rather than logging a string and and catch the error,
+// and use the Stack object to log a useful error message.
+//
+// 2) Abstract the test description to a string that's
+// passed to a generic function - 'test()'
