389de721bdf31d720a9491d69a0648e1b3333cd8
Luca <eto.lucafrancesco@gmail.com>
2018-12-10 16:37:06 +0000

    namespacing done

--- testFramework.js
+++ testFramework.js
@@ -1,25 +1,27 @@
-function Test () {
-}
+var run = {};
+(function(context) {
 
-Test.prototype.test_equal = function (argument, expectedValue) {
-  try {
-    if (isNaN(expectedValue)) throw new Error ("expected value is not a number")
-    if (argument !== expectedValue) throw new Error ("doesnt equal the expected value")
-    if (argument === expectedValue) console.log("%c - Pass", "color: green")
+  context.equal = function (argument, expectedValue) {
+    try {
+      if (isNaN(expectedValue)) throw new Error ("expected value is not a number")
+      if (argument !== expectedValue) throw new Error ("doesnt equal the expected value")
+      if (argument === expectedValue) console.log("%c - Pass", "color: green")
+      }
+    catch(err) {
+      console.log("%c - " + err.stack, "color: red")
     }
-  catch(err) {
-    console.log("%c - " + err.stack, "color: red")
   }
-}
 
-Test.prototype.test = function(func, message, argument, expectedvalue) {
-  console.log(message)
-  if (func === 'equal') {
-    this.test_equal(argument, expectedvalue)
+  context.test = function(func, message, argument, expectedvalue) {
+    console.log(message)
+    if (func === 'equal') {
+      this.equal(argument, expectedvalue)
+    }
   }
-}
+})
 
-var run = new Test();
+(run);
+
 var square = new Square(2)
 run.test('equal', 'it compares two values', square.area(), "four");
 run.test('equal', 'it compares two values', square.perimeter(), 8);
